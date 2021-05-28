c810bddc1bb4e67c1e077601a7bfc2cabff2d677
antonia <kourouniotou@uth.gr>
2021-05-28 21:42:18 +0300

    Updates

---
+++ precache-manifest.88746028fe280c9531e5581a80f77d54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d8d844abdf92cac942e52b1c8f421f",
+    "url": "/Auction_dapp/index.html"
+  },
+  {
+    "revision": "33123528c14d7568eb65",
+    "url": "/Auction_dapp/static/css/main.7004dac1.chunk.css"
+  },
+  {
+    "revision": "b7076fd3876b78dac419",
+    "url": "/Auction_dapp/static/js/2.13d998bb.chunk.js"
+  },
+  {
+    "revision": "33123528c14d7568eb65",
+    "url": "/Auction_dapp/static/js/main.9dce680d.chunk.js"
+  },
+  {
+    "revision": "754ba13c45fba147f1bb",
+    "url": "/Auction_dapp/static/js/runtime-main.28217a77.js"
+  }
+]);
